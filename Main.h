da1adc53c997e5fbd4ff9f4ebb8434edc3835a8a
Saurabh Jinturkar <saurabh.jinturkar@sjsu.edu>
2016-05-23 12:09:55 -0700

    benchmarking results

--- Main.h
+++ Main.h
@@ -9,7 +9,7 @@ extern Player players[10];
 //List of methods exposed to Python API Call
 Point getPlayerPositions(int i);
 Point getBallPosition();
-void init();
+void init(long time);
 long play(int threadNum);
 void stop();
 int getBoundaryBreadth();
